b2c8e4c7d7b14afbe9f7185cda6a260f7319646d
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2024-11-02 21:56:38 +0200

    feat: create service response class

---
+++ Models/ServiceResponse.cs
@@ -0,0 +1,11 @@
+namespace ASP_core_API.Models
+{
+    public class ServiceResponse<T>
+    {
+        public T? Data { get; set; }
+
+        public bool Success { get; set; } = true;
+
+        public string? Message { get; set; } = null;
+    }
+}
